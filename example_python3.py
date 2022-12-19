04427611c851d74e52de748b998f4afe688e9a3a
Amir <amirmohammadi00184@gmail.com>
2022-12-19 14:01:39 -0500

    modified example file

--- example_python3.py
+++ example_python3.py
@@ -1,49 +1,9 @@
-"""
-HX711 Load cell amplifier Python Library
-Original source: https://gist.github.com/underdoeg/98a38b54f889fce2b237
-Documentation source: https://github.com/aguegu/ardulibs/tree/master/hx711
-Adapted by 2017 Jiri Dohnalek
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-
-##################
-PYTHON 3 EXAMPLE
-
-This version of example is python 3 compatible
-and outputs weight in grams.
-
-Make sure you enter the correct values for offset and scale!
-Also, don't forget to set the correct gain, default is 128.
-"""
-
 import RPi.GPIO as GPIO
 import time
 import sys
 from hx711 import HX711
 
-# Force Python 3 ###########################################################
-
-if sys.version_info[0] != 3:
-    raise Exception("Python 3 is required.")
-
-############################################################################
-
-
 hx = HX711(5, 6)
-
 
 def cleanAndExit():
     print("Cleaning...")
@@ -56,8 +16,8 @@ def setup():
     """
     code run once
     """
-    hx.set_offset(8318456.625)
-    hx.set_scale(460.97727272727275)
+    hx.set_offset(8322072.5)
+    hx.set_scale(361.8913333333333)
 
 
 def loop():
@@ -78,7 +38,6 @@ def loop():
         cleanAndExit()
 
 
-##################################
 
 if __name__ == "__main__":
 
